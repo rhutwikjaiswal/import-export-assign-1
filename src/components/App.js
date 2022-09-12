d12bc6ab470bcb6a593796c61aab818d4a7d5761
Rhutwik jaiswal <103762165+rhutwikjaiswal@users.noreply.github.com>
2022-09-12 12:56:16 +0530

    aa

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,16 @@
 import React, { Component, useState } from "react";
 import '../styles/App.css';
-import Heading from './components/Heading';
-import InputQuery from './components/InputQuery';
-import SubHeading  from './components/SubHeading';
-import SubmitButton from './components/SubmitButton';
+import Heading from './Heading';
+import InputQuery from './InputQuery';
+import SubHeading  from './SubHeading';
+import SubmitButton from './SubmitButton';
  
 const App = () => {
   return (
     <div id="main">
       <Heading />
-      < InputQuery/>
-      < SubHeading/>
+      <InputQuery/>
+      <SubHeading/>
       <SubmitButton />
     </div>
   )
